4c0edba5459e7b9e06abc7c50cbad3a80481e20f
Carlos Daniel Maldonado Muñoz <201G0266@rcarbonifera.tecnm.mx>
2023-11-20 19:38:41 -0600

    a

--- BarbieQ/Repositories/Repository.cs
+++ BarbieQ/Repositories/Repository.cs
@@ -1,6 +1,47 @@
-﻿namespace BarbieQ.Repositories
+﻿using BarbieQ.Models.Entities;
+
+namespace BarbieQ.Repositories
 {
-    public class Repository
+    public class Repository<T> where T : class
     {
+        public BarbieAndCosmeticsContext Context { get; }
+        public Repository(BarbieAndCosmeticsContext context)
+        {
+            Context = context;
+        }
+        public virtual IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>();
+        }
+        public virtual T? Get(object id)
+        {
+            return Context.Find<T>(id);
+        }
+        public virtual void Insert(T entity)
+        {
+            Context.Add(entity);
+            Context.SaveChanges();
+        }
+        public virtual void Delete(T entity)
+        {
+            Context.Remove(entity);
+            Context.SaveChanges();
+        }
+        public virtual void Delete(object id)
+        {
+            var entity = Get(id);
+
+            if (entity != null)
+            {
+                Delete(entity);
+            }
+
+
+        }
+        public virtual void Update(T entity)
+        {
+            Context.Update(entity);
+            Context.SaveChanges();
+        }
     }
 }
