1c5755d8790287fdc840b9e988b02aa450d14e81
Carlos Daniel Maldonado Muñoz <201G0266@rcarbonifera.tecnm.mx>
2023-12-08 03:38:17 -0600

    Layout arreglado, creo

---
+++ BarbieQ/Models/ViewModels/LayoutViewModel.cs
@@ -0,0 +1,37 @@
+﻿using BarbieQ.Repositories;
+
+namespace BarbieQ.Models.ViewModels
+{
+    public class LayoutViewModel
+    {
+        public CategoriaRepository _catRepos { get; }
+        public LayoutViewModel(CategoriaRepository c)
+        {
+            _catRepos = c;
+        }
+        public int Contador { get; set; } = 0;
+        private int totalCategorias;
+        
+        public int TotalCategorias
+        {
+            get { return _catRepos.GetAll().Count(); }
+            set { totalCategorias = value; }
+        }
+        private IEnumerable<string> cat;
+
+        public IEnumerable<string> Categorias
+        {
+            get { return _catRepos.GetAll().Select(x=> x.Nombre); }
+            set { cat = value; }
+        }
+
+        private int mitad;
+
+        public int Mitad
+        {
+            get { return TotalCategorias/2; }
+            set { mitad = value; }
+        }
+
+    }
+}
